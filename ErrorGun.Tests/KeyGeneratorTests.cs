1d280f58d8bebb1bb100d6bddd9719914d08628f
Aidan Ryan <ryan.aidan@gmail.com>
2012-12-03 16:32:28 -0800

    Key gen unit tests

---
+++ ErrorGun.Tests/KeyGeneratorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using ErrorGun.Web.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ErrorGun.Tests
+{
+    [TestClass]
+    public class KeyGeneratorTests
+    {
+        [TestMethod]
+        public void KeyGenerator_GeneratesOneThousandUniqueKeys_WithValidFormat()
+        {
+            var hexRegex = new Regex(@"^[0-9a-z\-]*$", RegexOptions.Compiled);
+            var set = new HashSet<string>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var key = KeyGenerator.Generate();
+                Assert.IsTrue(hexRegex.IsMatch(key));
+                Assert.IsTrue(set.Add(key));
+            }
+        }
+    }
+}
