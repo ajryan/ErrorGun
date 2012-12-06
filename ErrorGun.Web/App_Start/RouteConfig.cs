3cc2b16483ce64f1eaae1520aeee77ab07ca08be
Aidan Ryan <ryan.aidan@gmail.com>
2012-12-06 15:46:15 -0800

    Homepage with Register and Edit sub-pages

--- ErrorGun.Web/App_Start/RouteConfig.cs
+++ ErrorGun.Web/App_Start/RouteConfig.cs
@@ -12,14 +12,9 @@ namespace ErrorGun.Web.App_Start
             routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
 
             routes.MapRoute(
-                name: RouteNames.DefaultAdmin,
-                url: "Admin/{action}",
-                defaults: new { controller = "Admin", action = "Index" });
-
-            routes.MapRoute(
                 name: RouteNames.Default,
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "App", action = "Create", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ErrorGun.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ErrorGun.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
